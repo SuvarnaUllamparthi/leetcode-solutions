39881d000c6c5d7baad9132238edeff41454f888
Suvarna ullamparthi <suvarnasuvarna50171@gmail.com>
2026-09-23 14:42:05 +0530

    Time: 4 ms (89.24%), Space: 12.6 MB (51.01%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        result = []
+        for num in range(left, right + 1):
+            temp = num
+            valid = True
+            while temp > 0:
+                digit = temp % 10
+                if digit == 0 or num % digit != 0:
+                    valid = False
+                    break
+                temp = temp // 10 
+            if valid:
+                result.append(num)
+        return result
